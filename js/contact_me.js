fe17c1d8fd3af558f7b4eb5704aa67beb0b48fce
cactus <522941939@qq.com>
2016-07-27 15:35:54 +0800

    CONTACT US 的邮件发送功能 完成
    用了 https://github.com/formspree/formspree

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "//formspree.io/yangjun.zhu@neulion.com.cn",
                 type: "POST",
                 data: {
                     name: name,
@@ -26,7 +26,6 @@ $(function() {
                     email: email,
                     message: message
                 },
-                cache: false,
                 success: function() {
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
